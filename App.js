06acd99a64b93cb2d770df7b66278a4c43769f1b
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-02-19 14:24:59 +0600

    add color, font, spacing..

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors'
+import { spacing } from './src/theme/spacing'
+import { typography } from './src/theme/typography'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+
+  if(!fontsLoaded) {
+    return <Text>Loading...</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: typography.primaryBold }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
