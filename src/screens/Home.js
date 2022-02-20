4262c374f429106615b9e9fa0aef7f208caea951
Marzan Islam <55044750+marzan999@users.noreply.github.com>
2022-02-20 13:24:55 +0600

    info add

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,45 @@
 import React from 'react'
-import { Pressable, View, StatusBar,  } from 'react-native'
+import { Pressable, View, StatusBar, FlatList} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Infoheader from '../components/info-header'
 import Text from '../components/text/text'
 import { colors } from '../theme/color'
 
+export const STUDENT_LIST = [
+
+{
+  name: 'marzan',
+  description: 'student',
+  image: require('../../assets/marzan.png')
+},
+{
+  name: 'nitish',
+  description: 'student',
+  // image: require('../../assets/nitish.PNG')
+   
+},
+{
+  name: 'maruf',
+  description: 'student',
+  // image: require('../../assets/maruf.PNG')
+}
+
+]
 
 export default function Home( {navigation} ) {
+  const renderItem = ({item, index}) => {
+    return (
+      <Text>{item.name}</Text>
+    )
+  }
   return (
     <SafeAreaView style={{backgroundColor: colors.white, flex: 1}} >
       <Infoheader />
-      <Pressable onPress={() => navigation.navigate("Details")}>
-        <Text>Go to Details</Text>
-      </Pressable>
+      <FlatList 
+        data={STUDENT_LIST}
+        renderItem = {renderItem}
+        keyExtractor = {(item, index) => item.name}
+      />
       <StatusBar barStyle='ligth-content' />
     </SafeAreaView>
   )
